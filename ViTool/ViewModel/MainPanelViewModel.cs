efeeca9b761643d3962989389725f3ca5c3f00c8
Mikolaj Karkowski <mikikarkowski@gmail.com>
2022-03-17 15:55:42 +0100

    Added new elements to MainPanel.xaml

--- ViTool/ViewModel/MainPanelViewModel.cs
+++ ViTool/ViewModel/MainPanelViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -11,7 +13,38 @@ namespace ViTool.ViewModel
 {
     public class MainPanelViewModel : ViewModelBase
     {
-        private string _DirectoryPath;
+
+        private ObservableCollection<string> _FilesList = new ObservableCollection<string>();
+        public ObservableCollection<string> FilesList
+        {
+            get { return _FilesList; }
+            set
+            {
+                if (_FilesList == value) return;
+
+                _FilesList = value;
+            }
+        }
+
+
+
+        public int ProgressPercent { get; set; }
+        public int EstimatedTime { get; set; }
+
+        private string _Output;
+        public string Output
+        {
+            get { return _Output; }
+            set
+            {
+                if (_Output == value)
+                    return;
+
+                _Output = value;
+            }
+        }
+
+        private string _DirectoryPath = "SomePath";
         public string DirectoryPath
         {
             get { return _DirectoryPath; }
@@ -54,10 +87,69 @@ namespace ViTool.ViewModel
             }
         }
 
-        public MainPanelViewModel(MirrorViewModel mirrorViewModel, TranslateXmlToTxTViewModel translateXmlToTxTViewModel) 
+        public SolidColorBrush TranslateXmlToTxTInfoBrush { get; set; }
+
+
+        public MainPanelViewModel(MirrorViewModel mirrorViewModel, TranslateXmlToTxTViewModel translateXmlToTxTViewModel)
         {
             _MirrorViewModel = mirrorViewModel;
             _TranslateXmlToTxTViewModel = translateXmlToTxTViewModel;
+            TranslateXmlToTxTInfoBrush = new SolidColorBrush(Color.FromRgb(220, 220, 220));
+        }
+
+        private RelayCommand _LoadData;
+        public RelayCommand LoadData
+        {
+            get
+            {
+                if (_LoadData == null)
+                {
+                    _LoadData = new RelayCommand(
+                     () =>
+                        {
+                            Output = "";
+                            FilesList.Clear();
+
+                            DirectoryPath = selectPath(DirectoryPath, "xd");
+                            if (DirectoryPath == null || DirectoryPath == "") return;
+
+                            DirectoryInfo d = new DirectoryInfo(DirectoryPath);
+
+                            DirectoryInfo[] directories = d.GetDirectories();
+                            foreach (DirectoryInfo directory in directories) FilesList.Add(directory.FullName);
+
+                            FileInfo[] Files = d.GetFiles();
+                            foreach (FileInfo file in Files) FilesList.Add(file.FullName);
+
+
+
+
+                        },
+                    () =>
+                    {
+                        return true;
+                    });
+                }
+
+                return _LoadData;
+            }
+        }
+        private string selectPath(string startingDir, string description)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.Description = description;
+            folderDlg.SelectedPath = startingDir;
+            folderDlg.ShowNewFolderButton = true;
+
+            DialogResult result = folderDlg.ShowDialog();
+
+            if (result != DialogResult.OK)
+                return null;
+
+            if (!Directory.Exists(folderDlg.SelectedPath))
+                return null;
+
+            return folderDlg.SelectedPath;
         }
     }
 }
