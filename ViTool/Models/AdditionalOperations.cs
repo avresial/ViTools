3fae0b6cbcbc772b3598ac952493deabc98178a5
Mikolaj Karkowski <mikikarkowski@gmail.com>
2022-03-17 21:27:21 +0100

    Code cleanup

---
+++ ViTool/Models/AdditionalOperations.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Xml;
+
+namespace ViTool.Models
+{
+    public static class AdditionalOperations
+    {
+
+        public static YoloObjectCounter CountDefects(string directoryWithXMLs)
+        {
+            YoloObjectCounter yoloObjectCounter = new YoloObjectCounter();
+
+            DirectoryInfo d = new DirectoryInfo(directoryWithXMLs);
+            FileInfo[] files = d.GetFiles("*.xml");
+            foreach (FileInfo file in files)
+                yoloObjectCounter.AddCounters(CountDefectsOnSingleFile(file.FullName));
+
+            return yoloObjectCounter;
+        }
+
+        private static YoloObjectCounter CountDefectsOnSingleFile(string pathToSingleXMLFile)
+        {
+            if (Path.GetExtension(pathToSingleXMLFile) != ".xml") return null;
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(pathToSingleXMLFile);
+
+            XmlNodeList nodes = doc.DocumentElement.SelectNodes("/annotation/object");
+
+            YoloObjectCounter yoloObjectCounter = ConvertXMLNodeToYoloObjectCounter(nodes);
+
+            return yoloObjectCounter;
+        }
+
+        private static YoloObjectCounter ConvertXMLNodeToYoloObjectCounter(XmlNodeList xmlNodeList)
+        {
+            YoloObjectCounter yoloObjectCounter = new YoloObjectCounter();
+
+            foreach (XmlNode node in xmlNodeList)
+            {
+                XmlNode objectNode = node.SelectSingleNode("name");
+                string defectName = objectNode.InnerText;
+                yoloObjectCounter.CountObject(defectName);
+            }
+
+            return yoloObjectCounter;
+        }
+
+        public static void DeleteObjectsWithNoDefects(string path)
+        {
+            DirectoryInfo d = new DirectoryInfo(path);
+            FileInfo[] files = d.GetFiles();
+            foreach (FileInfo file in files)
+                DeleteObject(file.FullName);
+        }
+        private static void DeleteObject(string xmlPath)
+        {
+
+            if (DeleteFileWithNoPair(xmlPath))
+                return;
+
+            string extension = Path.GetExtension(xmlPath);
+
+            if (extension != ".xml")
+                return;
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlPath);
+
+            var nodes = doc.DocumentElement.SelectNodes("/annotation/object");
+
+            if (nodes.Count != 0)
+                return;
+
+            if (File.Exists(xmlPath))
+            {
+                File.Delete(xmlPath);
+                Console.WriteLine("deleting - " + xmlPath);
+            }
+
+            if (File.Exists(xmlPath.Replace(extension, ".jpg")))
+            {
+                File.Delete(xmlPath.Replace(extension, ".jpg"));
+                Console.WriteLine("deleting - " + xmlPath.Replace(extension, ".jpg"));
+            }
+        }
+        private static bool DeleteFileWithNoPair(string path)
+        {
+            string extension = Path.GetExtension(path);
+
+            if (extension == ".xml")
+            {
+                if (!File.Exists(path.Replace(extension, ".jpg")))
+                {
+                    File.Delete(path);
+                    return true;
+                }
+            }
+
+
+            if (extension == ".jpg")
+            {
+                if (!File.Exists(path.Replace(extension, ".xml")))
+                {
+                    File.Delete(path);
+                    return true;
+
+                }
+            }
+
+            return false;
+        }
+
+
+        public static BitmapImage ToBitmapImage(System.Drawing.Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Jpeg);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+        }
+
+    }
+}
--- ViTool/ViewModel/MainPanelViewModel.cs
+++ ViTool/ViewModel/MainPanelViewModel.cs
@@ -23,23 +23,8 @@ namespace ViTool.ViewModel
         private IndicatorColors indicatorColors;
         private Settings settings;
 
-        private System.Drawing.Bitmap _Image;
-        public System.Drawing.Bitmap Image
-        {
-            get { return _Image; }
-            set
-            {
-                if (_Image == value)
-                    return;
-
-                _Image = value;
-                RaisePropertyChanged(nameof(Image));
-            }
-        }
-
         public string SelectedClass { get; set; }
         public string NewClass { get; set; }
-        public int FilesCount { get; set; }
 
         private string _SelectedFile;
         public string SelectedFile
@@ -71,26 +56,6 @@ namespace ViTool.ViewModel
             }
         }
 
-
-        public Progress<ProgressReportModel> progress;
-
-        public ObservableCollection<string> FilesList
-        {
-            get { return _FilesList; }
-            set
-            {
-                if (_FilesList == value) return;
-
-                _FilesList = value;
-            }
-        }
-        public ObservableCollection<string> ListOfClasses { get; set; }
-
-        public TranslateXmlToTxTAlgorithm TranslateXmlToTxT { get; set; }
-
-        public int ProgressPercent { get; set; }
-        public int EstimatedTime { get; set; }
-
         private string _Output;
         public string Output
         {
@@ -100,13 +65,13 @@ namespace ViTool.ViewModel
                 if (_Output == value)
                     return;
 
-                if (value != "") Img = null;
+                if (value != "") ImagePreview = null;
 
                 _Output = value;
             }
         }
 
-        private string _DirectoryPath = "SomePath";
+        private string _DirectoryPath = "";
         public string DirectoryPath
         {
             get { return _DirectoryPath; }
@@ -119,20 +84,43 @@ namespace ViTool.ViewModel
                 RaisePropertyChanged(nameof(DirectoryPath));
             }
         }
-        private ImageSource _Img;
-        public ImageSource Img
+
+
+        public int ProgressPercent { get; set; }
+        public int EstimatedTime { get; set; }
+
+
+        public ObservableCollection<string> FilesList
         {
-            get { return _Img; }
+            get { return _FilesList; }
             set
             {
-                if (_Img == value)
+                if (_FilesList == value) return;
+
+                _FilesList = value;
+            }
+        }
+        public ObservableCollection<string> ListOfClasses { get; set; } = new ObservableCollection<string>();
+
+        public TranslateXmlToTxTAlgorithm TranslateXmlToTxT { get; set; }
+
+        private ImageSource _ImagePreview;
+        public ImageSource ImagePreview
+        {
+            get { return _ImagePreview; }
+            set
+            {
+                if (_ImagePreview == value)
                     return;
 
-                _Img = value;
-                RaisePropertyChanged(nameof(Img));
+                _ImagePreview = value;
+                RaisePropertyChanged(nameof(ImagePreview));
             }
         }
 
+        public SolidColorBrush ProgressBrush { get; set; } = new SolidColorBrush(System.Windows.Media.Color.FromRgb(220, 220, 220));
+
+        public Progress<ProgressReportModel> progress;
 
         private MirrorViewModel _MirrorViewModel;
         public MirrorViewModel MirrorViewModel
@@ -148,40 +136,21 @@ namespace ViTool.ViewModel
             }
         }
 
-        private TranslateXmlToTxTViewModel _TranslateXmlToTxTViewModel;
-        public TranslateXmlToTxTViewModel TranslateXmlToTxTViewModel
-        {
-            get { return _TranslateXmlToTxTViewModel; }
-            set
-            {
-                if (_TranslateXmlToTxTViewModel == value)
-                    return;
 
-                _TranslateXmlToTxTViewModel = value;
-                RaisePropertyChanged(nameof(TranslateXmlToTxTViewModel));
-            }
-        }
-
-        public SolidColorBrush TranslateXmlToTxTInfoBrush { get; set; }
-
-
-        public MainPanelViewModel(MirrorViewModel mirrorViewModel, Progress<ProgressReportModel> progress, TranslateXmlToTxTViewModel translateXmlToTxTViewModel, IndicatorColors indicatorColors, TranslateXmlToTxTAlgorithm translateXmlToTxT, Settings settings)
+        public MainPanelViewModel(MirrorViewModel mirrorViewModel, Progress<ProgressReportModel> progress, IndicatorColors indicatorColors,
+            TranslateXmlToTxTAlgorithm translateXmlToTxT, Settings settings)
         {
             this.progress = progress;
             this.indicatorColors = indicatorColors;
             this.settings = settings;
             TranslateXmlToTxT = translateXmlToTxT;
             _MirrorViewModel = mirrorViewModel;
-            _TranslateXmlToTxTViewModel = translateXmlToTxTViewModel;
-            TranslateXmlToTxTInfoBrush = new SolidColorBrush(System.Windows.Media.Color.FromRgb(220, 220, 220));
-            ListOfClasses = new ObservableCollection<string>();
 
             SettingsData currentSettingsData = settings.ReadSettings();
             DirectoryPath = currentSettingsData.LastOpenedDirectory;
             foreach (string newClass in currentSettingsData.SavedClasses) ListOfClasses.Add(newClass);
-
-
         }
+
 
         private RelayCommand _LoadDataRelayCommand;
         public RelayCommand LoadDataRelayCommand
@@ -193,7 +162,7 @@ namespace ViTool.ViewModel
                     _LoadDataRelayCommand = new RelayCommand(
                      () =>
                         {
-                            DirectoryPath = selectPath(DirectoryPath, "xd");
+                            DirectoryPath = SelectPath(DirectoryPath, "Select Directory with jpg and/or xmls");
                             if (DirectoryPath == null || DirectoryPath == "") return;
                             LoadData();
 
@@ -221,19 +190,19 @@ namespace ViTool.ViewModel
                         Output = "";
 
                         SettingsData currentSettingsData = settings.ReadSettings();
-                        TranslateXmlToTxTInfoBrush = indicatorColors.busyColor;
+                        ProgressBrush = indicatorColors.busyColor;
 
                         if (DirectoryPath == null || DirectoryPath == "")
                         {
                             DirectoryPath = "No Directory";
-                            TranslateXmlToTxTInfoBrush = indicatorColors.errorColor;
+                            ProgressBrush = indicatorColors.errorColor;
                             Output += "There is no files";
                             return;
                         }
 
                         bool result = await Task.Run(() => TranslateXmlToTxT.TranslateXmlToTxTAsync(DirectoryPath, ".xml", ListOfClasses.ToList(), progress));
 
-                        TranslateXmlToTxTInfoBrush = result ? indicatorColors.doneColor : indicatorColors.errorColor;
+                        ProgressBrush = result ? indicatorColors.doneColor : indicatorColors.errorColor;
 
                         SaveSettings();
                         LoadData();
@@ -258,8 +227,7 @@ namespace ViTool.ViewModel
                     _AddClass = new RelayCommand(
                     () =>
                     {
-                        if (NewClass == null || NewClass == "")
-                            return;
+                        if (NewClass == null || NewClass == "") return;
 
                         ListOfClasses.Add(NewClass);
                         NewClass = "";
@@ -286,8 +254,7 @@ namespace ViTool.ViewModel
                     _DeleteClass = new RelayCommand(
                     () =>
                     {
-                        if (SelectedClass == null || SelectedClass == "")
-                            return;
+                        if (SelectedClass == null || SelectedClass == "") return;
 
                         ListOfClasses.Remove(SelectedClass);
 
@@ -315,10 +282,7 @@ namespace ViTool.ViewModel
                     {
                         ListOfClasses.Clear();
 
-                        SettingsData settingsData = new SettingsData();
-                        settingsData.SavedClasses = ListOfClasses.ToList();
-                        settingsData.LastOpenedDirectory = DirectoryPath;
-                        settings.SaveSettings(settingsData);
+                        SaveSettings();
                     },
                     () =>
                     {
@@ -340,7 +304,7 @@ namespace ViTool.ViewModel
                     _GetDatasetStatistics = new RelayCommand(
                     () =>
                     {
-                        Output = CountDefects(DirectoryPath).GetCounter();
+                        Output = AdditionalOperations.CountDefects(DirectoryPath).GetCounter();
                     },
                     () =>
                     {
@@ -352,7 +316,6 @@ namespace ViTool.ViewModel
             }
         }
 
-
         private RelayCommand _ClearData;
         public RelayCommand ClearData
         {
@@ -363,7 +326,7 @@ namespace ViTool.ViewModel
                     _ClearData = new RelayCommand(
                     () =>
                     {
-                        DeleteObjectsWithNoDefects(DirectoryPath);
+                        AdditionalOperations.DeleteObjectsWithNoDefects(DirectoryPath);
                         LoadData();
                     },
                     () =>
@@ -377,8 +340,7 @@ namespace ViTool.ViewModel
         }
 
 
-
-        private string selectPath(string startingDir, string description)
+        private string SelectPath(string startingDir, string description)
         {
             FolderBrowserDialog folderDlg = new FolderBrowserDialog();
             folderDlg.Description = description;
@@ -387,11 +349,9 @@ namespace ViTool.ViewModel
 
             DialogResult result = folderDlg.ShowDialog();
 
-            if (result != DialogResult.OK)
-                return null;
+            if (result != DialogResult.OK) return null;
 
-            if (!Directory.Exists(folderDlg.SelectedPath))
-                return null;
+            if (!Directory.Exists(folderDlg.SelectedPath)) return null;
 
             return folderDlg.SelectedPath;
         }
@@ -407,147 +367,14 @@ namespace ViTool.ViewModel
         {
             Output = "";
             FilesList.Clear();
-            FilesCount = 0;
 
             DirectoryInfo d = new DirectoryInfo(DirectoryPath);
 
-            DirectoryInfo[] directories = d.GetDirectories();
-            foreach (DirectoryInfo directory in directories) FilesList.Add(directory.Name);
-
             FileInfo[] Files = d.GetFiles();
             foreach (FileInfo file in Files) FilesList.Add(file.Name);
-
-            FilesCount += Files.Length;
-            FilesCount += directories.Length;
         }
 
-
-
-        public static YoloObjectCounter CountDefects(string directoryWithXMLs)
-        {
-            YoloObjectCounter yoloObjectCounter = new YoloObjectCounter();
-
-            DirectoryInfo d = new DirectoryInfo(directoryWithXMLs);
-            FileInfo[] files = d.GetFiles("*.xml");
-            foreach (FileInfo file in files)
-                yoloObjectCounter.AddCounters(CountDefectsOnSingleFile(file.FullName));
-
-            return yoloObjectCounter;
-        }
-
-        private static YoloObjectCounter CountDefectsOnSingleFile(string pathToSingleXMLFile)
-        {
-            if (Path.GetExtension(pathToSingleXMLFile) != ".xml") return null;
-
-            XmlDocument doc = new XmlDocument();
-            doc.Load(pathToSingleXMLFile);
-
-            XmlNodeList nodes = doc.DocumentElement.SelectNodes("/annotation/object");
-
-            YoloObjectCounter yoloObjectCounter = ConvertXMLNodeToYoloObjectCounter(nodes);
-
-            return yoloObjectCounter;
-        }
-
-        private static YoloObjectCounter ConvertXMLNodeToYoloObjectCounter(XmlNodeList xmlNodeList)
-        {
-            YoloObjectCounter yoloObjectCounter = new YoloObjectCounter();
-
-            foreach (XmlNode node in xmlNodeList)
-            {
-                XmlNode objectNode = node.SelectSingleNode("name");
-                string defectName = objectNode.InnerText;
-                yoloObjectCounter.CountObject(defectName);
-            }
-
-            return yoloObjectCounter;
-        }
-
-        public static void DeleteObjectsWithNoDefects(string path)
-        {
-            DirectoryInfo d = new DirectoryInfo(path);
-            FileInfo[] files = d.GetFiles();
-            foreach (FileInfo file in files)
-                DeleteObject(file.FullName);
-        }
-        private static void DeleteObject(string xmlPath)
-        {
-
-            if (DeleteFileWithNoPair(xmlPath))
-                return;
-
-            string extension = Path.GetExtension(xmlPath);
-
-            if (extension != ".xml")
-                return;
-
-            XmlDocument doc = new XmlDocument();
-            doc.Load(xmlPath);
-
-            var nodes = doc.DocumentElement.SelectNodes("/annotation/object");
-
-            if (nodes.Count != 0)
-                return;
-
-            if (File.Exists(xmlPath))
-            {
-                File.Delete(xmlPath);
-                Console.WriteLine("deleting - " + xmlPath);
-            }
-
-            if (File.Exists(xmlPath.Replace(extension, ".jpg")))
-            {
-                File.Delete(xmlPath.Replace(extension, ".jpg"));
-                Console.WriteLine("deleting - " + xmlPath.Replace(extension, ".jpg"));
-            }
-        }
-        private static bool DeleteFileWithNoPair(string path)
-        {
-            string extension = Path.GetExtension(path);
-
-            if (extension == ".xml")
-            {
-                if (!File.Exists(path.Replace(extension, ".jpg")))
-                {
-                    File.Delete(path);
-                    return true;
-                }
-            }
-
-
-            if (extension == ".jpg")
-            {
-                if (!File.Exists(path.Replace(extension, ".xml")))
-                {
-                    File.Delete(path);
-                    return true;
-
-                }
-            }
-
-            return false;
-        }
-
-
-        public BitmapImage ToBitmapImage(System.Drawing.Bitmap bitmap)
-        {
-            using (var memory = new MemoryStream())
-            {
-                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
-                memory.Position = 0;
-
-                var bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.StreamSource = memory;
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.EndInit();
-                bitmapImage.Freeze();
-
-                return bitmapImage;
-            }
-        }
-
-        void DrawRedRectangle()
+        private void DrawRedRectangle()
         {
             string fileName = Path.GetFileNameWithoutExtension(_SelectedFile) + ".xml";
 
@@ -570,7 +397,6 @@ namespace ViTool.ViewModel
                     int ymin = int.Parse(currentNode.SelectSingleNode("bndbox/ymin").InnerText);
                     int ymax = int.Parse(currentNode.SelectSingleNode("bndbox/ymax").InnerText);
 
-                    
                     Rectangle rectangle = new Rectangle();
                     rectangle.X = xmin;
                     rectangle.Y = ymin;
@@ -578,15 +404,17 @@ namespace ViTool.ViewModel
                     rectangle.Height = ymax - ymin;
 
                     string defectType = currentNode.SelectSingleNode("name").InnerText;
+
                     gr.DrawRectangle(pen, rectangle);
-                    gr.DrawString(s: defectType + ":    " , font: new Font(new Font("Times New Roman", 12.0f),
+                    gr.DrawString(s: defectType + ":    ", font: new Font(new Font("Times New Roman", 15.0f),
                         FontStyle.Bold), brush: new SolidBrush(pen.Color), point: new Point(rectangle.X, rectangle.Y + 25));
 
                 }
-
-                ;
+                gr.Dispose();
             }
-            Img = ToBitmapImage(bitmap);
+
+            ImagePreview = AdditionalOperations.ToBitmapImage(bitmap);
+            bitmap.Dispose();
 
         }
     }
